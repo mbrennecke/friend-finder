6f8d5afe2d5ad89ac80b7548a8a88cc005a58681
Matthew Brennecke <mbrennecke10@gmail.com>
2018-09-09 19:36:36 -0600

    got style

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, '/app/public/')));
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
 
 app.get("/index", function(req, res) {

841102e23d51b603b0e777c8db05b57561caf6bf
Matthew Brennecke <mbrennecke10@gmail.com>
2018-09-09 19:29:38 -0600

    it begins

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/index", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+
+
+
+// Starts the server to begin listening
+// ========================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
